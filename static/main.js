1e73def8f7e8062b5da8e0216fef49524776e8ac
thaihaluong <halt68.ftu@gmail.com>
2016-12-29 01:23:19 +0700

    Done!

--- static/main.js
+++ static/main.js
@@ -5,12 +5,6 @@
 
 //console.log("This is a log");
 
-var welcomeTag = document.getElementById("welcome");
-
-welcomeTag.innerHTML= "Welcome C4E7";
-
-
-
 
 function makedate(y,m,d) {
 
@@ -19,18 +13,6 @@ function makedate(y,m,d) {
 }
 
 function getDOB() {
-    var Leo={name: "Leo",desc: "Inspiring",img:"https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_leo.png"};
-    var Aries = {name: "Aries",desc: "Obnoxious", img:"https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_aries.png"};
-    var Taurus = {name:"Taurus",desc: "Steadfast", img: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_taurus.png"};
-    var Gemini = {name:"Gemini",desc: "Changeable", img: "http://www.mixbox365.com/wp-content/uploads/2016/10/az_img_horoscope_gemini.png"};
-    var Cancer = {name: "Cancer",desc: "Loyal", img: "http://www.mixbox365.com/wp-content/uploads/2016/10/az_img_horoscope_cancer.png"};
-    var Virgo = {name: "Virgo",desc: "Critical" ,img: "http://www.mixbox365.com/wp-content/uploads/2016/10/az_img_horoscope_virgo-1.png"};
-    var Libra = {name:"Libra",desc: "Uninteresting", img: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_libra.png"};
-    var Scorpio = {name:"Scorpio",desc: "Overated" ,img: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_scorpio.png"};
-    var Sagittarius = {name:"Sagittarius",desc: "Disloyal", img: "http://cdn.playbuzz.com/cdn/65d8ccdc-a332-47a3-83bd-3483ed4bd102/c5765556-b365-468f-8061-74aff941cc20.png"};
-    var Capricorn = {name:"Capricorn",desc: "Angelic", img: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_capricorn.png"};
-    var Aquarius = {name:"Aquarius",desc: "Genius", img: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_aquarius.png"};
-    var Pisces = {name:"Pisces",desc: "Underestimated" ,img: "http://cdn.playbuzz.com/cdn/65d8ccdc-a332-47a3-83bd-3483ed4bd102/66c582fd-e568-4444-94c2-bb802bc6a93c.png"};
 
     var day = document.getElementById("day").value;
     var month = document.getElementById("month").value - 1;
@@ -39,7 +21,7 @@ function getDOB() {
     //document.getElementById("result").innerHTML = makedate(year,8,23)
     //document.getElementById("try").innerHTML = date;
     if (makedate(year,7,23) <= date && date <= makedate(year,8,22)) {
-       document.getElementById("result").innerHTML = Leo.name;
+       document.getElementById("result").innerHTML = "Leo";
     }
     else if (makedate(year,8,23) <= date && date <= makedate(year,9,22)) {
         document.getElementById("result").innerHTML = "Virgo";
@@ -87,10 +69,9 @@ function getDOB() {
 
 }
 
-function getinfo() {
 
-    var horo = document.getElementById("result").value ;
-    document.getElementById("testing").innerHTML = horo;
+function getinfo() {
+    // list
     var Leo={name: "Leo",desc: "Inspiring",img:"https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_leo.png"};
     var Aries = {name: "Aries",desc: "Obnoxious", img:"https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_aries.png"};
     var Taurus = {name:"Taurus",desc: "Steadfast", img: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_taurus.png"};
@@ -106,19 +87,18 @@ function getinfo() {
 
     var array = [Leo,Aries,Taurus,Gemini,Cancer,Virgo,Libra,Scorpio,Sagittarius,Capricorn,Aquarius,Pisces];
 
+    var horo = document.getElementById("result").innerHTML;
 
     var x;
-    /*for (x=0;x<12;x++ ) {
+    var check = "";
+    for (x=0;check=="";x++ ) {
 
-        if (horo=array[x].name) {
+        if (horo==array[x].name) {
 
-            document.getElementById("desc").innerHTML = array[x].desc;
+            document.getElementById("desc").innerHTML = "Main character:" + array[x].desc;
             document.getElementById("pic").setAttribute("src", array[x].img);
-        }
+            check = document.getElementById("desc").innerHTML;}
 
-
-    }*/
-
-   document.getElementById("testing1").innerHTML = array[0].name;
+    }
 
 }
